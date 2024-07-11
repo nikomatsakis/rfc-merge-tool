bd1cd656812c0680f2162bbf0258d83e4ab5531f
Niko Matsakis <niko@alum.mit.edu>
2024-07-11 16:21:39 -0400

    some basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use octocrate::{APIConfig, GitHubAPI};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rfc-merge-tool", about = "A tool to merge RFCs.")]
+struct Opt {
+    /// Activate debug mode
+    // short and long flags (-d, --debug) will be deduced from the field's name
+
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    Pr { pr: i64 },
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let opt = Opt::from_args_safe()?;
+
+    match opt.command {
+        Command::Pr { pr } => {
+            // Create a default GitHub API configuration
+            let config = APIConfig::default().shared();
+
+            let api = GitHubAPI::new(&config);
+
+            let pull_request = api.pulls.get("rust-lang", "rfcs", pr).send().await.unwrap();
+
+            eprintln!("{pull_request:?}");
+        }
+    }
+
+    Ok(())
 }
